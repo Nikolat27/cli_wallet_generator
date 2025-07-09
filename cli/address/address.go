13cc935e13be7ca2e4b4aea19d1064c8179db12d
nikolat27 <samalizadeh899@gmail.com>
2025-07-09 19:34:30 +0330

    Added get list addresses func

--- cli/address/address.go
+++ cli/address/address.go
@@ -2,6 +2,7 @@ package address
 
 import (
 	"cli_wallet_generator/address"
+	"cli_wallet_generator/wallet"
 	"errors"
 	"fmt"
 )
@@ -11,22 +12,28 @@ const (
 	coinNameFlag   = "-c"
 )
 
+type Commands struct {
+	Address *wallet.Address
+}
+
 func HandleAddressCommands(inputs []string) error {
 	if len(inputs) < 2 {
 		return errors.New("missing subcommand for address (e.g., create, get, delete, list) ⚠️ ")
 	}
 
-	switch cmd := inputs[1]; cmd{
+	switch cmd := inputs[1]; cmd {
 	case "create":
 		return handleCreate(inputs)
+	case "list":
+		return handleList(inputs)
 	default:
 		return fmt.Errorf("unknown address subcommand: %s ❌  ", cmd)
 	}
 }
 
 func handleCreate(inputs []string) error {
-	if err := isCommandValid(inputs); err != nil {
-		return err
+	if len(inputs) < 6 || inputs[2] != walletNameFlag || inputs[3] == "" || inputs[4] != coinNameFlag || inputs[5] == "" {
+		return errors.New("invalid command. Usage: address create -w <walletName> -c <coinSymbol> ⚠️ ")
 	}
 
 	walletName := inputs[3]
@@ -37,12 +44,24 @@ func handleCreate(inputs []string) error {
 		return err
 	}
 
+	fmt.Printf("%s address created successfully! ✅ \n", coinName)
 	return nil
 }
 
-func isCommandValid(inputs []string) error {
-	if len(inputs) < 6 || inputs[2] != walletNameFlag || inputs[3] == "" || inputs[4] != coinNameFlag || inputs[5] == "" {
-		return errors.New("invalid subcommand for coinAddress (e.g., create, get, delete, list) ⚠️ ")
+func handleList(inputs []string) error {
+	if len(inputs) < 4 || inputs[2] != walletNameFlag || inputs[3] == "" {
+		return errors.New("invalid command. Usage: address list -w <walletName> ⚠️ ")
+	}
+
+	walletName := inputs[3]
+
+	addresses, err := address.RetrieveAddressList(walletName)
+	if err != nil {
+		return err
+	}
+
+	for idx, addr := range addresses {
+		fmt.Printf("%d. Coin: %s, Address: %s\n", idx+1, addr.Coin, addr.Address)
 	}
 
 	return nil
